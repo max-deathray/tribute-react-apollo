b5ba034c2edae5806dcfb23c5a585892fed8c7bc
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-21 15:41:20 -0500

    added little heart to each vibe

--- src/components/Vibe.js
+++ src/components/Vibe.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Button from '@material-ui/core/Button';
 
 class Vibe extends Component {
   render() {
@@ -7,7 +10,14 @@ class Vibe extends Component {
         <div>
           <img src={this.props.vibe.img} alt={this.props.vibe.description} />
         </div>
-        <div className="vibe-caption">{this.props.vibe.description}</div>
+        <div className="panel">
+          <div className="vibe-caption">{this.props.vibe.description}</div>
+          <div className="locket">
+            <Button>
+              <FavoriteBorder />
+            </Button>
+          </div>
+        </div>
       </div>
     );
   }
